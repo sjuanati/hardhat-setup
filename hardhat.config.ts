21866511beda018e9b244b4b81cc27a571054dc1
Sergi Juanati <wint3rmute@gmail.com>
2021-03-20 18:10:04 +0100

    First commit (incl. TypeScript adaptation)

---
+++ hardhat.config.ts
@@ -0,0 +1,11 @@
+// /**
+//  * @type import('hardhat/config').HardhatUserConfig
+//  */
+// module.exports = {
+//   solidity: "0.7.3",
+// };
+
+
+import { HardhatUserConfig } from "hardhat/types";
+const config: HardhatUserConfig = {};
+export default config;
