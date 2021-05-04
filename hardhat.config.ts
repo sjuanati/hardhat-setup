6e1df897669c0bead3c52235921f6b509fa75a1e
Sergi Juanati <wint3rmute@gmail.com>
2021-05-04 11:13:41 +0200

    Verify contract in Etherscan

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,18 +1,25 @@
 // it depends on hardhat-ethers, so adding both isn't necessary
 require("@nomiclabs/hardhat-waffle");
-// env variables
+// .env variables
 require('dotenv').config();
+// contract verification
+require("@nomiclabs/hardhat-etherscan");
+
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
-const MNEMONIC = process.env.MNEMONIC;
+const ETH_PRIVATE_KEY = process.env.ETH_PRIVATE_KEY;
+const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY;
 
 module.exports = {
     solidity: "0.7.6",
     networks: {
         ropsten: {
             url: `https://ropsten.infura.io/v3/${INFURA_API_KEY}`,
-            accounts: [`0x${MNEMONIC}`]
+            accounts: [`0x${ETH_PRIVATE_KEY}`]
         }
+    },
+    etherscan: {
+        apiKey: `${ETHERSCAN_KEY}`
     }
 };
 
